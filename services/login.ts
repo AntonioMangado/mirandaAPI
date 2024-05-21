e265c871dfbe16672012b5c2bd370002cf37fcad
Mangui <antonio.mangado@gmail.com>
2024-05-21 14:05:48 +0200

    ADD/ authentication now working with db

--- services/login.ts
+++ services/login.ts
@@ -1,6 +1,7 @@
 import { APIError } from "../middleware/error";
 import Admin from "../models/admins.models";
 import jwt from 'jsonwebtoken';
+import { compare } from 'bcrypt';
 
 
 export async function loginUser(data: {email:string, password:string}): Promise<string> {
@@ -9,12 +10,21 @@ export async function loginUser(data: {email:string, password:string}): Promise<
         throw new APIError("Please provide email and password", 400, true);
     }
 
-    if (email === "test@test.com" && password === "test") {
-        const user = { email, password };
-        const token = jwt.sign(user, process.env.CLIENT_SECRET as string, { expiresIn: "30m" });
-        return token;
+    const admin = await Admin.findOne({ email });
+    console.log(admin);
+    if (admin) {
+        const hashedPassword = admin.password;
+        console.log(hashedPassword);
+        const match = await compare(password, hashedPassword);
+        if (match) {
+            const user = { email, username: admin.username };
+            const token = jwt.sign(user, process.env.CLIENT_SECRET as string, { expiresIn: "30m" });
+            return token;
+        } else {
+            throw new APIError("Invalid email or password", 401, true);
+        }
     } else {
-        throw new APIError("Invalid email or password", 401, true);
+        throw new APIError("Invalid email", 401, true);
     }
 }
 
